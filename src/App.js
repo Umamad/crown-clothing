5e69f656a3d835dafd5c154370aa674f6f99c71f
Umamad <marandmohammad@gmail.com>
2021-06-20 16:25:17 +0430

    Learn React Routing and set it for Hats

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, Switch } from 'react-router';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const Hats = () => (
+  <div>
+    <h1>Hats</h1>
+  </div>
+);
+
 function App() {
   return (
+
     <div>
-      <HomePage />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={Hats} />
     </div>
   );
 }
