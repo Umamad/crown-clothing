deb1a7c635697455cf1d88343a3b37b92d898084
Umamad <marandmohammad@gmail.com>
2021-08-08 09:20:37 +0430

    End Cart reducer and add to cart action

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,11 @@
+export const addItemToCart = (cartItems, itemsToAdd) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === itemsToAdd.id
+    );
+    
+    if(existingCartItem){
+        return cartItems.map(cartItem => cartItem.id === itemsToAdd.id ? {...cartItem, quantity: cartItem.quantity +1 } : cartItem);
+    }
+
+    return [...cartItems, {...itemsToAdd, quantity: 1}];
+};
